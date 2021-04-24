854643f3132370ba91c73e24fd84983b11441d41
Guille <ggalmazor@gmail.com>
2021-04-24 11:27:30 +0200

    TDD: Red - single number in string

--- test/string_calculator_test.js
+++ test/string_calculator_test.js
@@ -3,3 +3,7 @@ import sc from '../src/string_calculator';
 test('An empty string produces 0', () => {
     expect(sc("")).toBe(0);
 });
+
+test('A string with a number produces that number', () => {
+    expect(sc("1")).toBe(1);
+})
