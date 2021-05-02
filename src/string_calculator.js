798c835941d812a168a244a98fef175e0bf38d78
Guille <ggalmazor@gmail.com>
2021-05-02 10:21:20 +0200

    TDD: Refactor - restructure to highligh moving and non-moving parts

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -2,21 +2,19 @@ export default input => {
   if (input === "")
     return 0;
 
+  let numbers;
   if (!input.startsWith("//")) {
     const normalizedInput = input.replaceAll("\n", ",");
-
     const parts = normalizedInput.split(',');
-
-    const numbers = parts.map(part => parseInt(part, 10))
-
-    return numbers.reduce((a, b) => a + b, 0);
+    numbers = parts.map(part => parseInt(part, 10))
+  } else {
+    const separators = [];
+    separators.push("\n");
+    separators.push(input[2]);
+    const normalizedInput = separators.reduce((body, separator) => body.replaceAll(separator, ","), input.substr(4));
+    const parts = normalizedInput.split(',');
+    numbers = parts.map(part => parseInt(part, 10))
   }
 
-  const separators = [];
-  separators.push("\n");
-  separators.push(input[2]);
-  const normalizedInput = separators.reduce((body, separator) => body.replaceAll(separator, ","), input.substr(4));
-  const parts = normalizedInput.split(',');
-  const numbers = parts.map(part => parseInt(part, 10))
   return numbers.reduce((a, b) => a + b, 0);
 }
