a034fc055cc4b8dfe3a3f5ae737c7813b0cfc1fa
Guille <ggalmazor@gmail.com>
2021-05-02 09:49:14 +0200

    TDD: Refactor - Generalize solution for map-reduce

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -7,9 +7,13 @@ export default input => {
 
   const partA = input[0];
   const partB = input[2];
+  const parts = [];
+  parts.push(partA);
+  parts.push(partB);
 
+  const numbers = parts.map(part => parseInt(part, 10))
   const numberA = parseInt(partA);
   const numberB = parseInt(partB);
 
-  return numberA + numberB;
+  return numbers.reduce((a, b) => a + b, 0);
 }
