ff791539325045bc5931c5786f7cdddb2124cfef
Guille <ggalmazor@gmail.com>
2021-05-02 10:03:11 +0200

    TDD: Red - "//;\n1:2"

--- test/string_calculator_test.js
+++ test/string_calculator_test.js
@@ -16,3 +16,8 @@ test('A string with numbers separated by commas produce the sum of those numbers
 test('Numbers can also be separated by the newline character', () => {
     expect(sc("1\n2")).toBe(3);
 });
+
+test('A custom delimited can be defined', () => {
+    expect(sc("//;\n1;2")).toBe(3);
+
+})
