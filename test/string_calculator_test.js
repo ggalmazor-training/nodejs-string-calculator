7195aa730ecd94297ebe403936607ca424b52cbb
Guille <ggalmazor@gmail.com>
2021-05-02 09:43:05 +0200

    TDD: Red - two numbers separated by comma

--- test/string_calculator_test.js
+++ test/string_calculator_test.js
@@ -8,3 +8,7 @@ test('A string with a number produces that number', () => {
     expect(sc("1")).toBe(1);
     expect(sc("11")).toBe(11);
 })
+
+test('A string with numbers separated by commas produce the sum of those numbers', () => {
+    expect(sc("1,2")).toBe(3);
+});
