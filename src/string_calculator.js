9edb1b6e8f2aa42382066985d56d2b196964b9c6
Guille <ggalmazor@gmail.com>
2021-05-02 10:15:53 +0200

    TDD: Refactor - Generalize for an array of arbitrary separators

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -12,10 +12,10 @@ export default input => {
     return numbers.reduce((a, b) => a + b, 0);
   }
 
-  const separator = input[2];
-  let normalizedInput = input.substr(4);
-  normalizedInput = normalizedInput.replaceAll(separator, ",");
-  normalizedInput = normalizedInput.replaceAll("\n", ",");
+  const separators = [];
+  separators.push("\n");
+  separators.push(input[2]);
+  const normalizedInput = separators.reduce((body, separator) => body.replaceAll(separator, ","), input.substr(4));
   const parts = normalizedInput.split(',');
   const numbers = parts.map(part => parseInt(part, 10))
   return numbers.reduce((a, b) => a + b, 0);
