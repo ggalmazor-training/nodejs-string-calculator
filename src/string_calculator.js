2e9109712d37c93fe03d8ff3277b698a08b9d33a
Guille <ggalmazor@gmail.com>
2021-05-02 10:01:00 +0200

    TDD: Refactor - Remove redundant if branch now that we're normalizing the newline chars

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -5,14 +5,6 @@ export default input => {
   if (input.indexOf(',') === -1 && input.indexOf("\n") === -1)
     return parseInt(input);
 
-  if (input.indexOf("\n") === -1) {
-    const parts = input.split(',');
-
-    const numbers = parts.map(part => parseInt(part, 10))
-
-    return numbers.reduce((a, b) => a + b, 0);
-  }
-
   const normalizedInput = input.replaceAll("\n", ",");
 
   const parts = normalizedInput.split(',');
