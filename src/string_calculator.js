4b39d31a8b3e8d1854e39bc7c16e88768d3d0194
Guille <ggalmazor@gmail.com>
2021-05-02 10:01:44 +0200

    TDD: Refactor - Remove redundant if branch now that the split can handle a single number

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -2,9 +2,6 @@ export default input => {
   if (input === "")
     return 0;
 
-  if (input.indexOf(',') === -1 && input.indexOf("\n") === -1)
-    return parseInt(input);
-
   const normalizedInput = input.replaceAll("\n", ",");
 
   const parts = normalizedInput.split(',');
