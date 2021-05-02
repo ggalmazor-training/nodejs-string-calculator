571e385ed2adbbc563016027dec86f6b8bbea66f
Guille <ggalmazor@gmail.com>
2021-05-02 09:43:47 +0200

    TDD: Green

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,6 +1,12 @@
 export default input => {
-    if (input === "")
-      return 0;
+  if (input === "")
+    return 0;
 
+  if (input.indexOf(',') === -1)
     return parseInt(input);
+
+  const partA = input[0];
+  const partB = input[2];
+
+  return parseInt(partA) + parseInt(partB);
 }
