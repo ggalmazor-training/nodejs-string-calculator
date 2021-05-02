7ddf059355643b1f6c4b74732af85a02c9b63643
Guille <ggalmazor@gmail.com>
2021-05-02 10:07:32 +0200

    TDD: Green

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -2,11 +2,22 @@ export default input => {
   if (input === "")
     return 0;
 
-  const normalizedInput = input.replaceAll("\n", ",");
+  if (!input.startsWith("//")) {
+    const normalizedInput = input.replaceAll("\n", ",");
 
+    const parts = normalizedInput.split(',');
+
+    const numbers = parts.map(part => parseInt(part, 10))
+
+    return numbers.reduce((a, b) => a + b, 0);
+  }
+
+  const separator = input[2];
+  const body = input.substr(4);
+  let normalizedInput = body;
+  normalizedInput = normalizedInput.replaceAll(separator, ",");
+  // normalizedInput = normalizedInput.replaceAll("\n", ",");
   const parts = normalizedInput.split(',');
-
   const numbers = parts.map(part => parseInt(part, 10))
-
   return numbers.reduce((a, b) => a + b, 0);
 }
